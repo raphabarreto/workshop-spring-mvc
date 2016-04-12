41c07768530f1cd54820ffee704634d131b82291
Raphael Barreto <tnt.raphael@gmail.com>
2016-04-12 19:09:34 -0300

    Enviando os dados para o servidor

---
+++ cobranca/src/main/java/com/workshop/cobranca/model/Titulo.java
@@ -0,0 +1,91 @@
+package com.workshop.cobranca.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
+@Entity
+public class Titulo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String descricao;
+
+	@Temporal(TemporalType.DATE)
+	private Date dataVencimento;
+
+	private BigDecimal valor;
+
+	@Enumerated(EnumType.ORDINAL)
+	private StatusTitulo titulo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public StatusTitulo getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(StatusTitulo titulo) {
+		this.titulo = titulo;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if ((obj == null) || (getClass() != obj.getClass())) {
+			return false;
+		}
+		Titulo other = (Titulo) obj;
+		return new EqualsBuilder().append(id, other.id).isEquals();
+	}
+}
