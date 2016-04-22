1f60fbd16a260fd4467541b31ba3ec320683a640
Raphael Barreto <tnt.raphael@gmail.com>
2016-04-22 02:14:23 -0300

    Implementando a edição

--- cobranca/src/main/java/com/workshop/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/workshop/cobranca/controller/TituloController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,20 +22,22 @@ import com.workshop.cobranca.repository.Titulos;
 @RequestMapping("/titulos")
 public class TituloController {
 
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
+
 	@Autowired
 	private Titulos titulos;
 
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 		mv.addObject(new Titulo());
 		return mv;
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {	
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
 		if (errors.hasErrors()) {
-			return "CadastroTitulo";
+			return CADASTRO_VIEW;
 		}
 
 		titulos.save(titulo);
@@ -47,6 +50,13 @@ public class TituloController {
 		List<Titulo> todosTitulos = titulos.findAll();
 		ModelAndView mv = new ModelAndView("PesquisaTitulos");
 		mv.addObject("titulos", todosTitulos);
+		return mv;
+	}
+
+	@RequestMapping("{id}")
+	public ModelAndView edicao(@PathVariable("id") Titulo titulo) {
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(titulo);
 		return mv;
 	}
 
