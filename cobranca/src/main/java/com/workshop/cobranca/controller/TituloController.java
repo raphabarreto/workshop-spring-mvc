e72ce50dadbef83ecc68def48ea24a3b1721b1e1
Raphael Barreto <tnt.raphael@gmail.com>
2016-04-22 23:08:52 -0300

    Plugin para calendário

--- cobranca/src/main/java/com/workshop/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/workshop/cobranca/controller/TituloController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
@@ -40,9 +41,14 @@ public class TituloController {
 			return CADASTRO_VIEW;
 		}
 
-		titulos.save(titulo);
-		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
-		return "redirect:/titulos/novo";
+		try {
+			titulos.save(titulo);
+			attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
+			return "redirect:/titulos/novo";
+		} catch (DataIntegrityViolationException e) {
+			errors.rejectValue("dataVencimento", null, "Formato de data inválido");
+			return CADASTRO_VIEW;
+		}
 	}
 
 	@RequestMapping
